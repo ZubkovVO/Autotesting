0e7583fdfd3233e26086a873d6f0e536baecd462
InquisitorBR <zubkov.v.o@gmail.com>
2018-05-09 15:14:21 +0400

    Hello

--- calc/src/main/java/TestCalc.java
+++ calc/src/main/java/TestCalc.java
@@ -1,7 +1,58 @@
+import java.util.Scanner;
+
 public class TestCalc {
 
+    private static Scanner keyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        double a, b, result = 0;
+        boolean check = false;
+        String oper;
+        String message = null;
+
+
+        System.out.println("enter first digit");
+        checkInputVal();
+        a = keyboard.nextDouble();
+        System.out.println("enter second digit");
+        checkInputVal();
+        b = keyboard.nextDouble();
+        System.out.println("enter operation");
+        oper = keyboard.next();
+        switch (oper) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+
+            default:
+                check = true;
+                message = "Operation impossible in this version";
+                break;
+
+        }
+        if (check == true) {
+            System.out.println(message);
+        } else {
+            System.out.println(result);
+
+        }
+    }
+
+    private static void checkInputVal(){
+        while (!keyboard.hasNextDouble()){
+            System.out.println("Please enter a number");
+            keyboard.next();
+        }
     }
 
 }
